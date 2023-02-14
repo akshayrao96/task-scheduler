052dd16dc2b0ad4aa210fa1c4ad15943d9159370
Akshay <akshayrao196@gmail.com>
2023-02-14 01:38:31 -0800

    turnaround and waiting times

--- question1.cc
+++ question1.cc
@@ -36,6 +36,15 @@ public:
   }
 };
 
+class cmpProcessID
+{
+public:
+  int operator()(const Process &p1, const Process &p2)
+  {
+    return p1.processID > p2.processID;
+  }
+};
+
 void errorMessage()
 {
   cout << "USAGE: Executable file must take in 1 valid file path" << endl;
@@ -55,6 +64,21 @@ void printOrder(queue<int> orderOfProcesses, map<int, int> IDFinishTime)
   cout << endl;
 }
 
+void updateTimes(vector<Process> &allProcessID, map<int, int> IDFinishTime)
+{
+  cout << endl;
+  cout << setw(4) << "ID" << setw(18) << "Turnaround time" << setw(15) << "Waiting time" << endl;
+  int i;
+  for (i = 0; i < allProcessID.size(); i++)
+  {
+    allProcessID[i].terminationTime = IDFinishTime[allProcessID[i].processID];
+    allProcessID[i].turnaroundTime = allProcessID[i].terminationTime - allProcessID[i].arrivalTime;
+    allProcessID[i].waitingTime = allProcessID[i].turnaroundTime - allProcessID[i].burstTime;
+    cout << setw(4) << allProcessID[i].processID << setw(12) << allProcessID[i].terminationTime << setw(18) << allProcessID[i].waitingTime << endl;
+  }
+  cout << endl;
+}
+
 int main(int argc, char **argv)
 {
   // Throws error if not given exactly 1 argument
@@ -67,6 +91,7 @@ int main(int argc, char **argv)
 
   // Priority queue to have processes in order of arrival time
   priority_queue<Process, vector<Process>, cmpProcessArrivalTime> arrivalQueue;
+  priority_queue<Process, vector<Process>, cmpProcessID> processIDqueue;
 
   ifstream schedulerFile;
   schedulerFile.open(argv[1]);
@@ -106,17 +131,25 @@ int main(int argc, char **argv)
     p.burstTime = burstTime;
     p.burstTimeLeft = burstTime;
     arrivalQueue.push(p);
+    processIDqueue.push(p);
   }
 
   schedulerFile.close(); // Finished reading file, close file
 
-  vector<Process> allProcesses; // List to hold all processes
+  vector<Process> allProcessArrivalTime; // List to hold all processes for scheduling to take place and iterating
+  vector<Process> allProcessID;          // List to hold all processes in id order, to iterate for termination, turnaround and waiting time
 
-  // Populates allProcesses and processList with all processes
+  // Populates allProcessArrivalTime and processList with all processes
   while (!arrivalQueue.empty())
   {
-    allProcesses.push_back(arrivalQueue.top()); // add to all processes
-    arrivalQueue.pop();                         // remove process 1 by 1 from priority queue
+    allProcessArrivalTime.push_back(arrivalQueue.top()); // add to all processes
+    arrivalQueue.pop();                                  // remove process 1 by 1 from priority queue
+  }
+
+  while (!processIDqueue.empty())
+  {
+    allProcessID.push_back(processIDqueue.top());
+    processIDqueue.pop();
   }
 
   // multiadaptive queues
@@ -124,7 +157,7 @@ int main(int argc, char **argv)
   queue<Process> q1;
   queue<Process> q2;
 
-  queue<int> orderOfProcesses; // order of process execution
+  queue<int> orderOfProcesses; // order of when process is executed
   map<int, int> IDFinishTime;  // maps id to termination time
 
   Process *current = NULL;
@@ -134,15 +167,15 @@ int main(int argc, char **argv)
   int timer = -1;
 
   // starts scheduling and processing each process, using counter as a timer
-  while (!allProcesses.empty() || !q0.empty() || !q1.empty() || !q2.empty())
+  while (!allProcessArrivalTime.empty() || !q0.empty() || !q1.empty() || !q2.empty())
   {
     counter += 1;
     timer -= 1;
 
-    while (!allProcesses.empty() && allProcesses.front().arrivalTime == counter)
+    while (!allProcessArrivalTime.empty() && allProcessArrivalTime.front().arrivalTime == counter)
     {
-      q0.push(allProcesses.front());
-      allProcesses.erase(allProcesses.begin());
+      q0.push(allProcessArrivalTime.front());
+      allProcessArrivalTime.erase(allProcessArrivalTime.begin());
       if (current == NULL)
       {
         current = &q0.front();
@@ -245,6 +278,7 @@ int main(int argc, char **argv)
     prev = current; // set prev as current after changes in scheduling either from timer or preempt process
   }
 
+  updateTimes(allProcessID, IDFinishTime);
   printOrder(orderOfProcesses, IDFinishTime);
 
   return 0;
